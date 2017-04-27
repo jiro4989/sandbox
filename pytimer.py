b5c5ef8f0096c27f334ba56231920a662e5cf476
jiro <gitromaru666>
2017-04-28 01:34:11 +0900

    プログラムの整理

--- pytimer.py
+++ pytimer.py
@@ -13,23 +13,6 @@ if len(sys.argv) < 2:
 		print(str(i) + ' - ' + a)
 	exit()
 
-# 色文字列生成関数を返す
-def getcolor(colorname):
-	colors = {
-			'clear'  : '\033[0m' ,
-			'black'  : '\033[40m',
-			'red'    : '\033[41m',
-			'green'  : '\033[42m',
-			'yellow' : '\033[43m',
-			'blue'   : '\033[44m',
-			'purple' : '\033[45m',
-			'cyan'   : '\033[46m',
-			'white'  : '\033[47m'
-			}
-	def f(c):
-		return colors[colorname] + c + colors['clear']
-	return f
-
 u"""
 タイムミリ秒を返す関数
 @param h 時間
@@ -48,14 +31,41 @@ u"""
 def int2(hm):
 	return (int(hm[0]), int(hm[1]))
 
-# 時間定数
-MINUTE = 60
-HOUR   = 60 * MINUTE
+# 色文字列生成関数を返す
+def getcolor(colorname):
+	colors = {
+			'clear'  : '\033[0m' ,
+			'black'  : '\033[40m',
+			'red'    : '\033[41m',
+			'green'  : '\033[42m',
+			'yellow' : '\033[43m',
+			'blue'   : '\033[44m',
+			'purple' : '\033[45m',
+			'cyan'   : '\033[46m',
+			'white'  : '\033[47m'
+			}
+	def f(c):
+		return colors[colorname] + c + colors['clear']
+	return f
 
 # 色文字列生成関数
 red   = getcolor('red')
 yello = getcolor('yello')
 green = getcolor('green')
+
+# 各種時間に対応した時間文字列を生成する
+def mk_time_text(hour, minute, second):
+	text = '%02d:%02d:%02d\r' % (hour, minute, second)
+	if hour <= 0 and minute <= 3:
+		return red(text)
+	elif hour <= 0 and minute <= 5:
+		return yellow(text)
+	else:
+		return green(text)
+
+# 時間定数
+MINUTE = 60
+HOUR   = 60 * MINUTE
 
 # csvファイルからタイマーのタイミングを読み込み
 timelines = open(sys.argv[1], 'r').readlines()
@@ -73,33 +83,26 @@ count = 0
 while True:
 	if len(timelines) < count:
 		break
+
 	nowtime = datetime.today()
 	nowtime = nowtime.timetuple()
 	nowtime = time.mktime(nowtime)
 
 	diff = nexttime - nowtime
 
-	hour   = diff / HOUR
-	hour   = int(hour)
-	minute = (diff % HOUR) / MINUTE
-	minute = int(minute)
-	second = ((diff % HOUR) % MINUTE)
-	second = int(second)
+	hour   = int(  diff / HOUR)
+	minute = int(( diff % HOUR) / MINUTE)
+	second = int(((diff % HOUR) % MINUTE))
 
-	dt = '%02d:%02d:%02d\r' % (hour, minute, second)
-	text = ''
+	# 色を変更して残り時間を標準出力
+	text = mk_time_text(hour, minute, second)
+	stdout.write(text)
+
 	if hour <= 0 and minute <= 0 and second <= 0:
 		count += 1
 		(h, m) = int2(timelines[count].split(':'))
 		nexttime = mktime(h,m)
-		text = red(dt)
-	elif hour <= 0 and minute <= 3:
-		text = red(dt)
-	elif hour <= 0 and minute <= 5:
-		text = yellow(dt)
-	else:
-		text = green(dt)
-	stdout.write(text)
+
 	sleep(1)
 
 print(green(u"""
