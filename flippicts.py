b23b22bb9237afe62f0b161f0f409cc379c9e438
jiro <gitromaru666>
2017-07-01 00:02:07 +0900

    コメント追加

--- flippicts.py
+++ flippicts.py
@@ -18,35 +18,37 @@ from PIL import Image
 from glob import glob
 from os import sep
 from os.path import join, relpath
-import os
-import re
-import sys
+import os, re, sys
 
-d = os.path.dirname(os.path.abspath(__file__))
-if 1 < len(sys.argv):
-	d = sys.argv[1]
-	d = d[0:-1]
+def main():
+    d = os.path.dirname(os.path.abspath(__file__))
+    if 1 < len(sys.argv):
+        d = sys.argv[1]
+        d = d[0:-1]
 
-rdir = d + sep + 'right'
-ldir = d + sep + 'left'
+    rdir = d + sep + 'right'
+    ldir = d + sep + 'left'
 
-if not os.path.exists(rdir):
-	print('rightディレクトリを作成しました。')
-	os.mkdir(rdir)
+    if not os.path.exists(rdir):
+        print('rightディレクトリを作成しました。')
+        os.mkdir(rdir)
 
-if not os.path.exists(ldir):
-	print('leftディレクトリを作成しました。')
-	os.mkdir(ldir)
+    if not os.path.exists(ldir):
+        print('leftディレクトリを作成しました。')
+        os.mkdir(ldir)
 
-rfiles    = [x for x in glob(join(rdir, '*.png'))]
-filenames = [relpath(x, rdir) for x in rfiles]
+    rfiles    = [x for x in glob(join(rdir, '*.png'))]
+    filenames = [relpath(x, rdir) for x in rfiles]
 
-for (rfile, filename) in zip(rfiles, filenames):
-	rimg = Image.open(rfile)
-	limg = rimg.transpose(Image.FLIP_LEFT_RIGHT)
-	filename = filename.replace('_R_', '_L_')
-	lname = ldir + sep + filename
-	limg.save(ldir + sep + filename)
-	print('[ {0} ] を出力しました。'.format(lname))
+    for (rfile, filename) in zip(rfiles, filenames):
+        rimg = Image.open(rfile)
+        limg = rimg.transpose(Image.FLIP_LEFT_RIGHT)
+        filename = filename.replace('_R_', '_L_')
+        lname = ldir + sep + filename
+        limg.save(ldir + sep + filename)
+        print('[ {0} ] を出力しました。'.format(lname))
 
-print('正常に出力が完了しました！')
+    print('正常に出力が完了しました！')
+
+if __name__ == '__main__':
+    main()
