c05e53a6611612d13a55d3b19d6212525fdac8d3
jiro <jiroron666@gmail.com>
2017-12-29 08:44:02 +0900

    用途不明のスクリプトを削除

--- special/dummy.py
+++
@@ -1,7 +0,0 @@
-for alpha in "abcdefghijklmnopqrstuvwxyz":
-    for i in range(1, 4):
-        index = "%03d" % i
-        filename = "{name}{index}.js".format(name=alpha, index=index)
-        print(filename)
-        with open(filename, "w") as f:
-            f.write("")
