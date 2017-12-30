b7bb22c51a1d7826ec420e07a3c0fbde976584d7
jiro4989 <jiroron666@gmail.com>
2017-12-30 11:43:44 +0900

    update env.sh for mkdir workspaces

--- setup/shell/env.sh
+++ setup/shell/env.sh
@@ -6,10 +6,18 @@ xset r rate 300 62
 # ユーザフォルダの日本語を英語に変更
 LANG=C xdg-user-dirs-gtk-update
 
-mkdir ~/Documents/note
-mkdir ~/workspace
-mkdir ~/workspace/proj
-git clone https://github.com/jiro4989/scripts.git ~/workspace/scripts
+github="$HOME/workspace/github"
+gitrepo="$github/repos"
+ds=(
+  "$github/gist/"
+  "$gitrepo/plugins/firefox"
+  "$gitrepo/plugins/vim"
+)
+for d in ${ds[@]}; do
+  mkdir -p $d
+done
+
+git clone https://github.com/jiro4989/scripts.git $gitrepo/scripts
 
 # ビープ音を無効化
 sudo echo "blacklist pcspkr" >> /etc/modprobe.d/blacklist.conf
