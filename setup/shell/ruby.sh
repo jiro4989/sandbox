4f398da7bc5d8bcd61064c4629f35dec79263b7a
jiro4989 <jiroron666@gmail.com>
2018-02-12 22:13:37 +0900

    railsの環境構築のコマンドを追加

--- setup/shell/ruby.sh
+++ setup/shell/ruby.sh
@@ -6,4 +6,6 @@ git clone git://github.com/sstephenson/rbenv.git $HOME/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
 sudo apt-get install ruby-dev
+sudo apt-get install libsqlite3-dev
 sudo gem install rails
+bundle install
