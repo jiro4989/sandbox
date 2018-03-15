6082909ff9288d0a3243ddc1698b95f7af52d71f
jiro4989 <jiroron666@gmail.com>
2018-03-15 23:35:03 +0900

    sudo apt-getって普通に引数でインストール対象片っ端から渡せるんやね...

--- setup/shell/other.sh
+++ setup/shell/other.sh
@@ -1,27 +1,26 @@
 #!/bin/bash
 
-otherapts=(
-  "krita"                   # paint tool
-  "kazam"                   # screen capture
-  "libav-tools imagemagick" # mp4 converter
-  "pandoc"                  # markdown convert to docx
-  "mysql-server"
-  "openssh-server"          # ssh remote access
-  "wine-stable"
-  "curl"
-  "synapse"                 # app launcher
-  "growisofs"               # burn ISO image
-  "clamav"                  # security scan
-  "screen"                  # split terminal screen
-  "xclip"                   # copy to clipboard
-  "unar"                    # unzip
-  "npm"
-  "zsh"
-  "terminator"
-)
-for apt in ${otherapts[@]}; do
-  sudo apt-get install $apt -y
-done
+sudo apt-get install -y \
+  krita \
+  kazam \
+  libav-tools \
+  imagemagick \
+  pandoc \
+  mysql-server \
+  openssh-server \
+  wine-stable \
+  curl \
+  synapse \
+  growisofs \
+  clamav \
+  screen \
+  xclip \
+  unar \
+  npm \
+  zsh \
+  terminator \
+  build-essential \
+  libgl1-mesa-dev
 
 # デフォルトターミナルの変更
 update-alternatives --config x-terminal-emulator
