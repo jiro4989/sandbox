5d8ee4b97dafbe804e745ab3962c0606f62d6c91
jiro4989 <jiroron666@gmail.com>
2018-03-24 22:26:23 +0900

    add util for making clitool

--- setup/shell/go.sh
+++ setup/shell/go.sh
@@ -25,3 +25,6 @@ go get -u google.golang.org/api/gmail/v1
 go get -u github.com/therecipe/qt
 go get -u -v github.com/therecipe/qt/cmd/..
 go get -d github.com/tcnksm/cli-init
+
+cd $GOPATH/src/github.com/tcnksm/cli-init
+go install
