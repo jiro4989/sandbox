a677f73368061ae195023d7eaa871134dec887a6
jiro4989 <jiroron666@gmail.com>
2018-03-24 12:10:02 +0900

    fix agg script

--- bash/agg_illust_count_per_year.sh
+++ bash/agg_illust_count_per_year.sh
@@ -3,13 +3,12 @@
 # 概要
 #   イラストディレクトリ内のイラストを書いた枚数を年単位で集計する
 
-find $HOME/Pictures/Picture/ -type f |
-  grep -vE "\.(png|jpg|txt|zip|exe|xml|lnk|~)$" |
-  xargs ls -l 2> /dev/null |
-  sort -k8n -k6n -k7n |
-  awk '{print $8"年", $6, $7"日", $9}' |
-  awk 'BEGIN{print "No","Count"} {map[$1]++} END { for (key in map) { sum+=map[key]; print key, map[key] }}' |
-  sort -k1n |
-  column -t |
-  tee >(awk '{sum+=$2} END{print "合計", sum"枚"}') |
+find $HOME/Pictures/Picture -type f |
+  grep -E '\.(sai|xcf|xpg)$' |
+  xargs ls --full-time 2>/dev/null |
+  awk '{print $6,$9}' |
+  sed -r 's@^(.*)-[0-9]+-[0-9]+ @\1 @g' |
+  awk '{m[$1]++} END{for (key in m){print key,m[key]}}' |
+  sort |
+  awk 'BEGIN{print "date","count"; print "-------", "---"} {print $0; sum+=$2} END{print "-------", "---"; print "total", sum}' |
   column -t
