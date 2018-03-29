61606694d5561f4759bc8ccd6e9aabd013a63d8e
jiro4989 <jiroron666@gmail.com>
2018-03-29 21:18:40 +0900

    add color

--- setup/shell/go.sh
+++ setup/shell/go.sh
@@ -23,6 +23,7 @@ go get -u google.golang.org/api/gmail/v1
 go get -u github.com/therecipe/qt
 go get -u -v github.com/therecipe/qt/cmd/..
 go get github.com/ChimeraCoder/gojson/gojson
+go get github.com/fatih/color
 
 go get -d github.com/tcnksm/gcli
 cd $GOPATH/src/github.com/tcnksm/gcli
