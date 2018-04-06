edca08bdd7af4fab26ee821b3b5c6ff0da140753
jiro4989 <jiroron666@gmail.com>
2018-04-06 20:33:42 +0900

    add chmod

--- setup/shell/docker.sh
+++ setup/shell/docker.sh
@@ -4,3 +4,4 @@ set -eu
 
 sudo apt-get install -y docker.io
 sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
