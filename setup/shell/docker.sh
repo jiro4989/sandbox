e29583584ad35881dcbc0c40e4957f860de9165d
jiro4989 <jiroron666@gmail.com>
2018-04-07 16:31:38 +0900

    sudo権限なしでdockerコマンドを実行できるようにした

--- setup/shell/docker.sh
+++ setup/shell/docker.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-set -eu
+# set -eu
 
 sudo apt-get install -y docker.io
+sudo groupadd docker
+sudo gpasswd -a $USER docker
+
 sudo curl -L https://github.com/docker/compose/releases/download/1.20.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
