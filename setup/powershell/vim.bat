6afa797a0692946e94d6647916ec90e2e2c5b26a
jiro4989 <jiroron666@gmail.com>
2018-04-30 23:45:11 +0900

    vimの環境構築スクリプトを修正した

--- setup/powershell/vim.bat
+++ setup/powershell/vim.bat
@@ -1,20 +1,5 @@
 @echo off
 
-rem 各種tmpファイルの配置場所の作成
-mkdir %userprofile%\tmp
-
 git clone https://github.com/jiro4989/dotfiles.git %userprofile%\dotfiles
 git clone https://github.com/Shougo/dein.vim.git %userprofile%\dotfiles\vim\dein
-
-mklink /d %userprofile%\.vim %userprofile%\dotfiles\vim\
-
-rem 各種rcファイルのシンボリックリンクの追加
-mklink %userprofile%\.vimrc %userprofile%\.vim\rc\.vimrc
-mklink %userprofile%\.gvimrc %userprofile%\.vim\rc\.gvimrc
-mklink %userprofile%"\.vimperatorrc" %userprofile%"\.vim\rc\.vimperatorrc"
-mklink %userprofile%"\.vrapperrc" %userprofile%"\.vim\rc\.vrapperrc"
-
-rem tomlファイルの配置場所の作成
-mkdir  %userprofile%\.config
-mkdir  %userprofile%\.config\vim
-mklink /d %userprofile%\.config\vim\dein %userprofile%\.vim\rc\dein\
+mklink /d %userprofile%\vimfiles %userprofile%\dotfiles\vim\
--- setup/shell/vim.sh
+++ setup/shell/vim.sh
@@ -1,34 +1,9 @@
 #!/bin/bash
 
-mkdir ~/tmp
+sudo apt-get install -y vim
+sudo apt-get install -y vim-gnome
 
-# vimのインストール {{{
-apts=(
-  "vim"
-  "vim-gnome"
-)
-for apt in ${apts[@]}; do
-  sudo apt-get install $apt -y
-done
-#}}}
-# 必要リポジトリのcloneの取得 {{{
 github="https://github.com"
 git clone $github/jiro4989/dotfiles.git ~/dotfiles
 git clone $github/Shougo/dein.vim.git ~/dotfiles/vim/dein
 ln -sf ~/dotfiles/vim/ ~/.vim
-#}}}
-# rcファイルのシンボリックリンクの作成 {{{
-rcs=(
-  ".vimrc"
-  ".gvimrc"
-  ".vimperatorrc"
-  ".vrapperrc"
-)
-for rc in ${rcs[@]}; do
-  ln -sf ~/.vim/rc/$rc ~/
-done
-#}}}
-# tomlファイルの配置 {{{
-mkdir -p ~/.config/vim
-ln -sf ~/.vim/rc/dein ~/.config/vim/dein
-#}}}
