a9d82f5b37956a459243bf68039fa939336eb0bd
jiro4989 <jiroron666@gmail.com>
2018-04-30 14:09:55 +0900

    update

--- setup/shell/java.sh
+++ setup/shell/java.sh
@@ -1,20 +1,7 @@
 #!/bin/bash
 
-# java
-# ------------------------------------------------------------
-#sudo apt-get update -y
-#sudo apt-get install software-properties-common python-software-properties -y
-#sudo add-apt-repository ppa:webupd8team/java -y
-#sudo apt-get install oracle-java8-installer -y
-
-javaapts=(
-  "junit4"
-  "ant"
-  "maven"
-  "openjdk-8-jdk"
-  "openjfx"
-)
-for apt in ${javaapts[@]}; do
-  sudo apt-get install $apt -y
-done
-
+sudo apt-get install -y junit4
+sudo apt-get install -y ant
+sudo apt-get install -y maven
+sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -y openjfx
