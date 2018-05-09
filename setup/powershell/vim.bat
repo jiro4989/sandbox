36df6353f82c12abc695d1fcf35d1f22af74d6fe
jiro4989 <jiroron666@gmail.com>
2018-05-10 07:20:57 +0900

    セットアップスクリプト修正

--- setup/powershell/vim.bat
+++ setup/powershell/vim.bat
@@ -1,5 +1,6 @@
 @echo off
 
 git clone https://github.com/jiro4989/dotfiles.git %userprofile%\dotfiles
-git clone https://github.com/Shougo/dein.vim.git %userprofile%\dotfiles\vim\dein
+mkdir %userprofile%\dotfiles\bundle
+git clone https://github.com/VundleVim/Vundle.vim.git %userprofile%\dotfiles\bundle\Vundle.vim
 mklink /d %userprofile%\vimfiles %userprofile%\dotfiles\vim\
--- setup/shell/vim.sh
+++ setup/shell/vim.sh
@@ -5,5 +5,6 @@ sudo apt-get install -y vim-gnome
 
 github="https://github.com"
 git clone $github/jiro4989/dotfiles.git ~/dotfiles
-git clone $github/Shougo/dein.vim.git ~/dotfiles/vim/dein
+mkdir ~/dotfiles/bundle
+git clone $github/VundleVim/Vundle.vim.git ~/dotfiles/bundle/Vundle.vim
 ln -sf ~/dotfiles/vim/ ~/.vim
