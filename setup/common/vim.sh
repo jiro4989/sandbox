86f5f4e279bbeb8f935221feee0c532e23468b02
jiro4989 <jiroron666@gmail.com>
2018-08-28 23:12:22 +0900

    不要なディレクトリ作成のコマンドを削除した。

--- setup/common/vim.sh
+++ setup/common/vim.sh
@@ -3,9 +3,6 @@
 GIT=https://github.com
 MY_GITHUB=$GIT/jiro4989
 DOTFILES_DIR=$HOME/dotfiles
-BUNDLE_DIR=$DOTFILES_DIR/vim/bundle
 
 git clone $MY_GITHUB/dotfiles.git $DOTFILES_DIR
-mkdir $BUNDLE_DIR
-git clone $GIT/VundleVim/Vundle.vim.git $BUNDLE_DIR/Vundle.vim
 ln -sf $DOTFILES_DIR/vim/ $HOME/.vim
