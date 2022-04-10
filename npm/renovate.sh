0999083b96e7078ce3b5e794b9aef35ec0963970
jiro4989 <jiroron666@gmail.com>
2022-04-10 10:43:42 +0900

    fix

--- npm/renovate.sh
+++ npm/renovate.sh
@@ -4,5 +4,6 @@ set -eu
 
 docker run \
   -e RENOVATE_TOKEN="${RENOVATE_TOKEN}" \
+  -v /tmp:/tmp -v /var/run/docker.sock:/var/run/docker.sock \
   --rm -it renovate/renovate:slim \
   jiro4989/sandbox
